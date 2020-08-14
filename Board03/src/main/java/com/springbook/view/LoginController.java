f96ac79faaf173d6ea03614ccebd6bafcba1ef3c
DaeHyeon-Hong <fholng@outlook.com>
2020-08-14 17:48:00 +0900

    hh

--- Board03/src/main/java/com/springbook/view/LoginController.java
+++ Board03/src/main/java/com/springbook/view/LoginController.java
@@ -1,11 +1,14 @@
 package com.springbook.view;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.springbook.biz.user.UserVO;
+import com.springbook.biz.user.impl.UserDAO;
 
 @Controller /* Controller */
 public class LoginController {
@@ -15,6 +18,21 @@ public class LoginController {
 		System.out.println("==> 로그인 화면으로 이동");
 		vo.setId("test");
 		vo.setPassword("test1234");
+		return "login.jsp";
+	}
+
+	/* ID, Password 값이 설정된 UserVO가 넘어 옴 */
+	@RequestMapping(value = "/login.do", method = RequestMethod.POST)
+	public String login(UserVO vo, UserDAO userDAO, HttpSession session) {
+		if (vo.getId() == null || vo.getId().equals("")) {
+			throw new IllegalArgumentException("아이디는 반드시 입력해야 합니다.");
+		}
+		UserVO user = userDAO.getUser(vo);
+		if (user != null) {
+			session.setAttribute("useName", user.getName());
+			System.out.printf("userName: %s", user.getName());
+			return "getBoardList.do";
+		}
 		return "login.jsp";
 	}
 

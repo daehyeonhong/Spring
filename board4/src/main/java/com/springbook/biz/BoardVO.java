256608d680ab75c855b9ecb08ea168785cc6657a
DaeHyeon-Hong <fholng@outlook.com>
2020-08-24 20:20:32 +0900

    08/24/Mon Spring

--- board4/src/main/java/com/springbook/biz/BoardVO.java
+++ board4/src/main/java/com/springbook/biz/BoardVO.java
@@ -1,12 +1,10 @@
 package com.springbook.biz;
 
 import java.sql.Date;
-
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlAttribute;
 import javax.xml.bind.annotation.XmlTransient;
-
 import org.springframework.web.multipart.MultipartFile;
 
 @XmlAccessorType(XmlAccessType.FIELD)
@@ -29,75 +27,91 @@ public class BoardVO {
 	private String searchCondition;
 	@XmlTransient
 	private String searchKeyword;
-	
+
 	public String getImages() {
 		return images;
 	}
+
 	public void setImages(String images) {
 		this.images = images;
 	}
+
 	public MultipartFile getUploadFile() {
 		return uploadFile;
 	}
+
 	public void setUploadFile(MultipartFile uploadFile) {
 		this.uploadFile = uploadFile;
 	}
-	
+
 	public String getSearchCondition() {
 		return searchCondition;
 	}
+
 	public void setSearchCondition(String searchCondition) {
 		this.searchCondition = searchCondition;
 	}
+
 	public String getSearchKeyword() {
 		return searchKeyword;
 	}
+
 	public void setSearchKeyword(String searchKeyword) {
 		this.searchKeyword = searchKeyword;
 	}
+
 	public int getSeq() {
 		return seq;
 	}
+
 	public void setSeq(int seq) {
 		this.seq = seq;
 	}
+
 	public String getTitle() {
 		return title;
 	}
+
 	public void setTitle(String title) {
 		this.title = title;
 	}
+
 	public String getWriter() {
 		return writer;
 	}
+
 	public void setWriter(String writer) {
 		this.writer = writer;
 	}
+
 	public String getContent() {
 		return content;
 	}
+
 	public void setContent(String content) {
 		this.content = content;
 	}
+
 	public Date getRegDate() {
 		return regDate;
 	}
+
 	public void setRegDate(Date regDate) {
 		this.regDate = regDate;
 	}
+
 	public int getCnt() {
 		return cnt;
 	}
+
 	public void setCnt(int cnt) {
 		this.cnt = cnt;
 	}
-	
+
 	@Override
 	public String toString() {
 		return "BoardVO [seq=" + seq + ", title=" + title + ", writer=" + writer + ", content=" + content + ", regDate="
 				+ regDate + ", cnt=" + cnt + "]";
 	}
-	
-	
 
 }
